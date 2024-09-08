11865ca2f300ff2627de6f47b3e06bf922316e57
Shivangi Singh <shivangisingh0306@gmail.com>
2024-09-08 15:32:30 +0530

    eComm-Application

---
+++ routes/auth.routes.js
@@ -0,0 +1,13 @@
+//POST localhost:8888/ecomm/api/v1/auth/signup
+
+//I need to intercept this
+
+const authController = require("../controllers/auth.controller")
+const authMW = require("../middlewares/auth.mw")
+
+module.exports = (app)=>{
+    app.post("/ecomm/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
+
+    //Route for POST localhost:8888/ecomm/api/v1/auth/signin
+    app.post("/ecomm/api/v1/auth/signin", [authMW.verifySignInBody], authController.signin)
+}
